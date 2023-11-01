7cdad8012e96c59f9af1c2b1de3eec1b3a513eb1
Blair <blairjohnson562@gmail.com>
2023-11-01 09:26:36 -0500

    Finished

--- functions_over_http/app/views.py
+++ functions_over_http/app/views.py
@@ -1,14 +1,14 @@
 from django.http.response import HttpResponse
 
-# Create your views here.
+# This function asks for name then returns 'Hey, (name)'.
 def hello_view(request, name):
     return HttpResponse(f"Hey, {name}!")
     
-
+# This has three arguments. one is the standard https request and the other two are asking for a random year then your birth year. it will return the sum of the random year minus your birth year.
 def age_view(request, end, birthyear):
     sum = end - birthyear
     return HttpResponse(sum)
-    
+#  This will calculate and return the total of burgers, drinks, or fried inputed throught the url.  
 def order_total(request, burgers, fries, drinks):
     GoodBurgers1 = (4.50 * burgers)
     FrenchFries1 = (1.50 * fries)
--- functions_over_http/config/urls.py
+++ functions_over_http/config/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+# This shows python where to go
 from app.views import hello_view, age_view, order_total
-
+#This tells the webpage where to go and what to request and respond with
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hey/<name>/', hello_view),
